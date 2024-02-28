a91a596a21d7a9e9f382a564cb03063c79073727
nishanprime <nt554@drexel.edu>
2024-02-28 14:27:43 -0500

    fixed tag controller exporting issue on main

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -1,5 +1,6 @@
 import AuthController from "./authController";
 import ClothController from "./clothController";
 import PostController from "./postController";
-export {  AuthController, ClothController, PostController };
 import TagController from "./tagController";
+
+export { AuthController, ClothController, PostController, TagController };
